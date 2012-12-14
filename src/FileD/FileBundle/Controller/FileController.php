0d2f8161740e07ef2de7e5ac70ed0346c6c1c56e
Eric Pidoux <eric.pidoux@free.fr>
2012-12-14 17:39:49 +0100

    Tried to fixed download for huge file

--- src/FileD/FileBundle/Controller/FileController.php
+++ src/FileD/FileBundle/Controller/FileController.php
@@ -708,7 +708,7 @@ class FileController extends Controller
 	    		
 	    		$response = new Response();
 	    		$response->setStatusCode(200);
-	    		$response->headers->set('Content-Type', "application/force-download".$mime);
+	    		$response->headers->set('Content-Type', "application/force-download");
 	    		$response->headers->set('Content-Transfer-Encoding', $mime."\n");
 	    		$response->headers->set('Content-Disposition', 'attachment; filename='.$name);
 	    		$response->headers->set('Content-Length', filesize($path));
